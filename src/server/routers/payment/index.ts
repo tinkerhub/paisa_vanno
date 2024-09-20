8cf8c11aac3849145726f893a12a63f529a7e006
muadpn <muadpn434@gmail.com>
2024-09-21 04:38:01 +0530

    feat: added infinity query, to have a input and a total displayed

--- src/server/routers/payment/index.ts
+++ src/server/routers/payment/index.ts
@@ -1,16 +1,40 @@
 import { z } from "zod";
 import { publicProcedure, router } from "../trpc";
 import { db } from "@/db";
+import { TRPCError } from "@trpc/server";
 
 export const appRouter = router({
-  getPayment: publicProcedure
+  getPaymentTotal: publicProcedure
     .input(
       z.object({
-        text: z.string(),
+        limit: z.number().optional(),
+        cursor: z.number().optional(),
       })
     )
-    .query(async (opts) => {
-      const data = await db.payments. 
+    .query(async ({ ctx, input }) => {
+      const { cursor } = input;
+      const totalAmount = await db.payments.aggregate({
+        _sum: {
+          recievedAmount: true,
+        },
+      });
+
+      const data = await db.payments.findMany({
+        cursor: cursor ? { id: cursor } : undefined,
+        take: 20,
+      });
+      if (!data.length) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Something went wrong",
+        });
+      }
+      let nextCursor = 1;
+      return {
+        response: data,
+        totalAmount: totalAmount._sum,
+        nextCursor,
+      };
     }),
 });
 
