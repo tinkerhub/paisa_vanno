c3639ef049911d74004fa8568d68250e795a0d52
muadpn <muadpn434@gmail.com>
2024-09-21 14:30:18 +0530

    merged: main to develop

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,4 +1,5 @@
 import { db } from "@/db";
+import { NextResponse } from "next/server";
 function getRandomNumber() {
   return Math.floor(Math.random() * (10000 - 100 + 1)) + 100;
 }
@@ -7,8 +8,11 @@ export async function POST() {
     await db.payments.create({
       data: {
         recievedAmount: getRandomNumber(),
-        paymentId: `hello+world ${getRandomNumber()}`
+        paymentId: `hello+world ${getRandomNumber()}`,
       },
     });
-  } catch (error) {}
+    return NextResponse.json({ message: true }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: true }, { status: 200 });
+  }
 }
