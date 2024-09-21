8cf5be6ac9b12fd7bf43f3d65cb7610fec98d26a
unknown <neilnicholaskanat@gmail.com>
2024-09-21 08:16:35 +0530

    feat(ui): improve responsiveness and add confetti effect

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function ProgressBar() {
   const { currCount, totalCount } = displayData;
   return (
-    <div className="bg-[#DADADA] max-md:mt-40 max-md:h-16 h-20 tv:h-52 w-[90%] mx-auto rounded-full mt-10 tv:mt-20 relative">
+    <div className="bg-[#d4d1d1] max-md:mt-40 max-md:h-16 h-20 tv:h-52 w-[90%] mx-auto rounded-full mt-10 tv:mt-20 relative shadow-2xl">
       <div className="bg-[#004BFF] max-md:h-16 h-20 tv:h-52 w-[80.5%] rounded-full"></div>
       <div className="absolute right-0 max-md:-top-36 -top-40 tv:-top-80 text-center text-responsive-num font-bold">
         <p className="text-7xl tv:text-9xl">⛳️</p>
